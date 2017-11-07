5d1d4e79399a5ec4cbaecbba6edd819a8c2bb79c
Yan.Shen <sheny308@yahoo.com>
2017-11-07 10:18:28 -0500

    dynamic Route

--- app.js
+++ app.js
@@ -1,22 +1,51 @@
 const express = require('express');
 const app = express();
 const nunjucks = require('nunjucks');
+const routes = require('./routes');
+var morgan = require('morgan')
 
+/////////////////////////////////////////////////////////////////////////////////////////////////
+// nunjucks.configure('views', {
+// 	autoescape: true,
+// 	express: app,
+// 	noCache: true
+// });
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views'); // point nunjucks to the proper directory for templates
+nunjucks.configure('views', { noCache: true })
+////////////////////////////////////////////////////////////////////////////////////////////////
+app.listen(3000);
 
+app.use(morgan('combined'));
+app.use(express.static('public'))
 
-app.listen(3000, function() {
-	console.log('listening on port 3000');
-});
+app.use('/', routes);
+app.get('/user/:id', function (req, res){
+	console.log(req.params.product +' ' + req.params.id)
+})
+// app.use('/', function(req, res, next) {
+// 	console.log(req.method + ' ' + req.originalUrl);
+// 	next();
+// });
 
-app.use('/', function(req, res, next) {
-	// console.log(req.url);
-	// console.log(req.)
-	console.log(req.method + ' ' + req.url);
-	next();
-});
+// app.get('/', function(req, res) {
+// 	var locals = {
+// 		title: 'An Example',
+// 		people: [
+// 				{ name: 'Gandalf'},
+// 				{ name: 'Frodo' },
+// 				{ name: 'Hermione'}
+// 		]
+// 	};
+// 	// res.render('index',  locals, function(err, output) {
+// 	//   res.send(output);
+// 	// });
+	// res.render('index',locals )
+// })
 
-app.get('/', function(req, res) {
-	res.send('your route is working..');
-});
+// 	nunjucks.render('index.html', locals)
+// })
 
+// in some file that is in the root directory of our application... how about app.js?
